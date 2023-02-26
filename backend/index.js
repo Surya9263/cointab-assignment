133b55c3a0e75f91c7367b8af409c6cbb5fa7fee
Surya Srivastava <suryasrivastava724@gmail.com>
2023-02-27 00:31:36 +0530

    backend done

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,21 @@
 const express = require("express");
-const axios = require("axios");
-const connect = require("./src/configs/db");
+// const connect = require("./src/configs/db");
+
+require("dotenv").config();
+
+const userRoute = require("./src/features/users/user.route.js");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  try {
-    const resp = await axios.get("https://randomuser.me/api/?results=100");
-    res.send(resp.data.results);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+app.use("/users", userRoute);
 
-app.listen(5000, async () => {
-  await connect();
-  console.log("server started on port 8080");
+app.get("/", (req, res) => res.send("hello"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, async () => {
+  console.log("server started on port 5000");
 });
---
+++ backend/src/features/users/user.route.js
@@ -0,0 +1,88 @@
+const { MongoClient, ObjectId } = require("mongodb");
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = client.db("cointab");
+
+async function connect() {
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    console.log("Connected to MongoDB!");
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+connect();
+
+const { Router, request } = require("express");
+const { default: axios } = require("axios");
+
+const router = Router();
+
+const getUsers = async () => {
+  try {
+    const res = await axios.get("https://randomuser.me/api/?results=100");
+    const data = res.data.results;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// GET all users API
+
+router.get("/", async (req, res) => {
+  const data = await getUsers()
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => console.log(err));
+
+  try {
+    await db.collection("users").drop();
+    const users = await db.collection("users").insertMany(data);
+
+    res.send(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+// DELETE all users API
+
+router.delete("/", async (req, res) => {
+  try {
+    await db.collection("users").drop();
+    res.send("Users deleted successfully");
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+// GET single user by id API
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await db
+      .collection("users")
+      .find({ _id: new ObjectId(id) })
+      .toArray();
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
